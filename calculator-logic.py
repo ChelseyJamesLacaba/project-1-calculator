c1f21adf2fc1f0d276e9c2314b929aff072d7945
DanielaSalamana <86301290+DanielaSalamana@users.noreply.github.com>
2021-07-02 17:47:53 +0800

    Update calculator-logic.py

--- calculator-logic.py
+++ calculator-logic.py
@@ -1,1 +1,18 @@
 # Calculator Logic
+
+#--------------------------------------- FUNCTIONS ---------------------------------------
+
+def about():
+    messagebox.showinfo('About'," \n CALCULATOR \n Created by NOT A PRO CODERS")
+
+def click_button(item):                                           # the 'click_button' function updates the input field constantly when you enter a number
+    global expression
+    input_text.set(input_text.get()+(str(item)))
+
+def clear_button():                                               # the 'clear_button' function removes the final item from the input list
+    global expression
+    expression = " "
+    input_text.set(input_text.get()[0:-1])
+
+def clear_all():                                                  # the 'clear_all' function removes all the item in the input field
+    input_text.set(" ")
