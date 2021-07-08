9e7d298d5c7a8072f0a270ea84137025393d439d
DanielaSalamana <86301290+DanielaSalamana@users.noreply.github.com>
2021-07-08 23:49:56 +0800

    Update calculator-logic.py

--- calculator-logic.py
+++ calculator-logic.py
@@ -2,26 +2,18 @@
 
 #--------------------------------------- FUNCTIONS ---------------------------------------
 
-def about():
-    messagebox.showinfo('About'," \n CALCULATOR \n Created by NOT A PRO CODERS")
+class Calculator:
+    def __init__(self, input_text):
+        self.input_text = input_text
 
-def click_button(item):                                           # the 'click_button' function updates the input field constantly when you enter a number
-    global expression
-    input_text.set(input_text.get()+(str(item)))
+    def about(self):
+        messagebox.showinfo('About'," \n CALCULATOR \n Created by NOT A PRO CODERS")
 
-def clear_button():                                               # the 'clear_button' function removes the last inputted item from the input list
-    global expression
-    expression = " "
-    input_text.set(input_text.get()[0:-1])
+    def click_button(self, item):                                           # the 'click_button' function updates the input field constantly when you enter a number
+        global expression
+        input_text.set(input_text.get()+(str(item)))
 
-def clear_all():                                                  # the 'clear_all' function removes all the item in the input field
-    input_text.set(" ")
-
-def equal_button():
-    result = " "
-    try:
-        result = eval(input_text.get())                           # 'eval' function dynamically evalutes the string expression
-        input_text.set(result)
-    except:
-        result = "ERROR"
-        input_text.set(result)
+    def clear_button(self):                                               # the 'clear_button' function removes the final item from the input list
+        global expression
+        expression = " "
+        input_text.set(input_text.get()[0:-1]                      
