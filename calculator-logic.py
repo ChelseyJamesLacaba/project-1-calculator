3a2b96ae7788e2035538ace0bf55894d7e0559ca
ChelseyJamesLacaba <86301287+ChelseyJamesLacaba@users.noreply.github.com>
2021-07-08 23:54:20 +0800

    Update calculator-logic.py

--- calculator-logic.py
+++ calculator-logic.py
@@ -9,7 +9,7 @@ class Calculator:
     def about(self):
         messagebox.showinfo('About'," \n CALCULATOR \n Created by NOT A PRO CODERS")
 
-    def click_button(self, item):                                           # the 'click_button' function updates the input field constantly when you enter a number
+    def click_button(self, item):                                         # the 'click_button' function updates the input field constantly when you enter a number
         global expression
         input_text.set(input_text.get()+(str(item)))
 
@@ -17,3 +17,20 @@ class Calculator:
         global expression
         expression = " "
         input_text.set(input_text.get()[0:-1]                      
+        
+    def clear_all(self):                                                  # the 'clear_all' function removes all the item in the input field
+        input_text.set(" ")
+
+    def equal_button(self):
+
+        result = " "
+        try:
+            result = eval(input_text.get())                              # 'eval' function dynamically evalutes the string expression
+            input_text.set(result)
+            return result
+        except:
+            result = "ERROR"
+            input_text.set(result)
+            return result
+
+c = Calculator(input_text)
