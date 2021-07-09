518e142076a0771f6bff44612ea08773c7f71187
JeraldTin-ao <86301336+JeraldTin-ao@users.noreply.github.com>
2021-07-09 11:48:08 +0800

    Update calculator-ui.py

--- calculator-ui.py
+++ calculator-ui.py
@@ -53,26 +53,26 @@ sqrt = Button(button_frame, text = "√", fg = "black", width = 10, height = 3, 
 divide_= Button(button_frame, text = "÷", fg = "black", width = 10, height = 3, bd = 0, bg = "orange2", cursor = "hand2", command = lambda: c.click_button("/")).grid(row = 2, column = 3, padx = 1, pady = 1)
 
 #Third_row
-seven = Button(button_frame, text = "7", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(7)).grid(row = 3, column = 0, padx = 1, pady = 1)
-eight = Button(button_frame, text = "8", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(8)).grid(row = 3, column = 1, padx = 1, pady = 1)
-nine = Button(button_frame, text = "9", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(9)).grid(row = 3, column = 2, padx = 1, pady = 1)
-multiply = Button(button_frame, text = "*", fg = "black", width = 10, height = 3, bd = 0, bg = "orange2", cursor = "hand2", command = lambda: click_button("*")).grid(row = 3, column = 3, padx = 1, pady = 1)
+seven = Button(button_frame, text = "7", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: c.click_button(7)).grid(row = 3, column = 0, padx = 1, pady = 1)
+eight = Button(button_frame, text = "8", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: c.click_button(8)).grid(row = 3, column = 1, padx = 1, pady = 1)
+nine = Button(button_frame, text = "9", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: c.click_button(9)).grid(row = 3, column = 2, padx = 1, pady = 1)
+multiply = Button(button_frame, text = "*", fg = "black", width = 10, height = 3, bd = 0, bg = "orange2", cursor = "hand2", command = lambda: c.click_button("*")).grid(row = 3, column = 3, padx = 1, pady = 1)
 
 #Fourth_row
-four = Button(button_frame, text = "4", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(4)).grid(row = 4, column = 0, padx = 1, pady = 1)
-five = Button(button_frame, text = "5", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(5)).grid(row = 4, column = 1, padx = 1, pady = 1)
-six = Button(button_frame, text = "6", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(6)).grid(row = 4, column = 2, padx = 1, pady = 1)
-minus = Button(button_frame, text = "-", fg = "black", width = 10, height = 3, bd = 0, bg = "orange2", cursor = "hand2", command = lambda: click_button("-")).grid(row = 4, column = 3, padx = 1, pady = 1)
+four = Button(button_frame, text = "4", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: c.click_button(4)).grid(row = 4, column = 0, padx = 1, pady = 1)
+five = Button(button_frame, text = "5", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: c.click_button(5)).grid(row = 4, column = 1, padx = 1, pady = 1)
+six = Button(button_frame, text = "6", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: c.click_button(6)).grid(row = 4, column = 2, padx = 1, pady = 1)
+minus = Button(button_frame, text = "-", fg = "black", width = 10, height = 3, bd = 0, bg = "orange2", cursor = "hand2", command = lambda: c.click_button("-")).grid(row = 4, column = 3, padx = 1, pady = 1)
 
 #Fifth_row
-one = Button(button_frame, text = "1", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(1)).grid(row = 5, column = 0, padx = 1, pady = 1)
-two = Button(button_frame, text = "2", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(2)).grid(row = 5, column = 1, padx = 1, pady = 1)
-three = Button(button_frame, text = "3", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(3)).grid(row = 5, column = 2, padx = 1, pady = 1)
-plus = Button(button_frame, text = "+", fg = "black", width = 10, height = 3, bd = 0, bg = "orange2", cursor = "hand2", command = lambda: click_button("+")).grid(row = 5, column = 3, padx = 1, pady = 1)
+one = Button(button_frame, text = "1", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: c.click_button(1)).grid(row = 5, column = 0, padx = 1, pady = 1)
+two = Button(button_frame, text = "2", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: c.click_button(2)).grid(row = 5, column = 1, padx = 1, pady = 1)
+three = Button(button_frame, text = "3", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: c.click_button(3)).grid(row = 5, column = 2, padx = 1, pady = 1)
+plus = Button(button_frame, text = "+", fg = "black", width = 10, height = 3, bd = 0, bg = "orange2", cursor = "hand2", command = lambda: c.click_button("+")).grid(row = 5, column = 3, padx = 1, pady = 1)
 
 #Sixth_row
-point = Button(button_frame, text = ".", fg = "white", width = 10, height = 3, bd = 0, bg = "gray25", cursor = "hand2", command = lambda: click_button(".")).grid(row = 6, column = 0, padx = 1, pady = 1)
-zero = Button(button_frame, text = "0", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(0)).grid(row = 6, column = 1, padx = 1, pady = 1)
-equals = Button(button_frame, text = "=", fg = "white", width = 21, height = 3, bd = 0, bg = "gray25", cursor = "hand2", command = lambda: equal_button()).grid(row = 6, column = 2, columnspan = 2, padx = 1, pady = 1)
+point = Button(button_frame, text = ".", fg = "white", width = 10, height = 3, bd = 0, bg = "gray25", cursor = "hand2", command = lambda: c.click_button(".")).grid(row = 6, column = 0, padx = 1, pady = 1)
+zero = Button(button_frame, text = "0", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: c.click_button(0)).grid(row = 6, column = 1, padx = 1, pady = 1)
+equals = Button(button_frame, text = "=", fg = "white", width = 21, height = 3, bd = 0, bg = "gray25", cursor = "hand2", command = lambda: c.equal_button()).grid(row = 6, column = 2, columnspan = 2, padx = 1, pady = 1)
  
 calc.mainloop()                       #mainloop() is an infinite loop used to run the application
