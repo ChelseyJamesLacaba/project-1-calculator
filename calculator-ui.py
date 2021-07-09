096469356620a869b93ec38d287c997a764cdcaf
VenusBaliguat <86301171+VenusBaliguat@users.noreply.github.com>
2021-07-09 11:41:31 +0800

    Update calculator-ui.py

--- calculator-ui.py
+++ calculator-ui.py
@@ -27,7 +27,7 @@ expression = ""
 input_text = StringVar() # 'StringVar()' is used to get the instance of input field
 
 # Input Frame
-input_frame = Frame(calc, width=312, height=50, bd=0, highlightbackground = "#121212", highlightcolor="crimson", highlightthickness=2)
+input_frame = Frame(calc, width=312, height=50, bd=0, highlightbackground="#121212", highlightcolor="crimson", highlightthickness=2)
 input_frame.pack(side=TOP)
 input_field = Entry(input_frame, font=('arial', 18), textvariable=input_text, width=50,fg="white", bg="#121212", bd=0, justify=RIGHT)
 input_field.grid(row=0, column=0)
@@ -36,22 +36,21 @@ input_field.pack(ipady=19)
 #Button Frame
 button_frame = Frame(calc, width=312, height=272.5, bg="grey")
 button_frame.pack()
-
 #--------------------------------------- BUTTONS ---------------------------------------
 
 #First_row
-clearall = Button(button_frame, text = "AC", fg = "white", width = 10, height = 3, bd = 0, bg = "gray25", cursor = "hand2", command = lambda: clear_all()).grid(row = 1, column = 0, padx = 1, pady = 1)
-l_parenth = Button(button_frame, text = "(", fg = "black", width = 10, height = 3, bd = 0, bg = "gray70", cursor = "hand2", command = lambda: click_button("(")).grid(row = 1, column = 1, padx = 1, pady = 1)
-r_parenth = Button(button_frame, text = ")", fg = "black", width = 10, height = 3, bd = 0, bg = "gray70", cursor = "hand2", command = lambda: click_button(")")).grid(row = 1, column = 2, padx = 1, pady = 1)
-clear = Button(button_frame, text = "DEL", fg = "white", width = 10, height = 3, bd = 0, bg = "gray25", cursor = "hand2", command = lambda: clear_button()).grid(row = 1, column = 3, padx = 1, pady = 1)
+clearall = Button(button_frame, text = "AC", fg = "white", width = 10, height = 3, bd = 0, bg = "gray25", cursor = "hand2", command = lambda: c.clear_all()).grid(row = 1, column = 0, padx = 1, pady = 1)
+l_bracket= Button(button_frame, text = "(", fg = "black", width = 10, height = 3, bd = 0, bg = "gray70", cursor = "hand2", command = lambda: c.click_button("(")).grid(row = 1, column = 1, padx = 1, pady = 1)
+r_bracket = Button(button_frame, text = ")", fg = "black", width = 10, height = 3, bd = 0, bg = "gray70", cursor = "hand2", command = lambda: c.click_button(")")).grid(row = 1, column = 2, padx = 1, pady = 1)
+clear = Button(button_frame, text = "DEL", fg = "white", width = 10, height = 3, bd = 0, bg = "gray25", cursor = "hand2", command = lambda: c.clear_button()).grid(row = 1, column = 3, padx = 1, pady = 1)
 
 #Second_row
 #With the Additional Functions
 #For 'pie_func' and 'sqrt_func', input a given int first before the said two functions
-power_func = Button(button_frame, text = "^", fg = "black", width = 10, height = 3, bd = 0, bg = "gray70", cursor = "hand2", command = lambda: click_button("**")).grid(row = 2, column = 0, padx = 1, pady = 1)
-pie_func = Button(button_frame, text = "π", fg = "black", width = 10, height = 3, bd = 0, bg = "gray70", cursor = "hand2", command = lambda: click_button( "*(3.1415)")).grid(row = 2, column = 1, padx = 1, pady = 1)
-sqrt_func = Button(button_frame, text = "√",fg = "black", width = 10, height = 3, bd = 0, bg = "gray70", cursor = "hand2", command = lambda: click_button("**(1/2)")).grid(row = 2, column = 2, padx = 1, pady = 1)
-divide_func = Button(button_frame, text = "/", fg = "black", width = 10, height = 3, bd = 0, bg = "orange2", cursor = "hand2", command = lambda: click_button("/")).grid(row = 2, column = 3, padx = 1, pady = 1)
+power = Button(button_frame, text = "^", fg = "black", width = 10, height = 3, bd = 0, bg = "gray70", cursor = "hand2", command = lambda: c.click_button("**")).grid(row = 2, column = 0, padx = 1, pady = 1)
+pie = Button(button_frame, text = "π", fg = "black", width = 10, height = 3, bd = 0, bg = "gray70", cursor = "hand2", command = lambda: c.click_button( "*(3.1415)")).grid(row = 2, column = 1, padx = 1, pady = 1)
+sqrt = Button(button_frame, text = "√", fg = "black", width = 10, height = 3, bd = 0, bg = "gray70", cursor = "hand2", command = lambda: c.click_button("**0.5")).grid(row = 2, column = 2, padx = 1, pady = 1)
+divide_= Button(button_frame, text = "÷", fg = "black", width = 10, height = 3, bd = 0, bg = "orange2", cursor = "hand2", command = lambda: c.click_button("/")).grid(row = 2, column = 3, padx = 1, pady = 1)
 
 #Third_row
 seven = Button(button_frame, text = "7", fg = "white", width = 10, height = 3, bd = 0, bg = "#121212", cursor = "hand2", command = lambda: click_button(7)).grid(row = 3, column = 0, padx = 1, pady = 1)
